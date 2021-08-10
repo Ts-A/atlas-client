56bb08cad1fe75d9bb03d036d4131c29868cd855
T.S.Ajeet <codewithstatus200@gmail.com>
2021-08-10 11:25:09 +0530

    feat: added login modal and backdrop

--- src/components/Forms/login.js
+++ src/components/Forms/login.js
@@ -1,5 +1,33 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './login.css';
+
+const Backdrop = () => {
+  return <div className="backdrop"></div>;
+};
+
+const LoginModal = (props) => {
+  const { toggleShowLogin, loginUser } = props;
+
+  return <Login toggleShowLogin={toggleShowLogin} loginUser={loginUser} />;
+};
+
+const LoginForm = (props) => {
+  const { toggleShowLogin, loginUser } = props;
+
+  return (
+    <React.Fragment key="login-form">
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById('react-backdrop')
+      )}
+      {ReactDOM.createPortal(
+        <LoginModal toggleShowLogin={toggleShowLogin} loginUser={loginUser} />,
+        document.getElementById('react-modal')
+      )}
+    </React.Fragment>
+  );
+};
 
 const Login = (props) => {
   const { toggleShowLogin, loginUser } = props;
@@ -39,46 +67,48 @@ const Login = (props) => {
   const [error, setError] = useState(false);
 
   return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      <div className="header">
-        <div>
-          Login
-          <span
-            className="material-icons"
-            style={{ fontSize: '32px', color: '#af1100' }}
-          >
-            room
+    <React.Fragment key="login">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="header">
+          <div>
+            Login
+            <span
+              className="material-icons"
+              style={{ fontSize: '32px', color: '#af1100' }}
+            >
+              room
+            </span>
+          </div>
+          <span className="material-icons close" onClick={handleOnClose}>
+            close
           </span>
         </div>
-        <span className="material-icons close" onClick={handleOnClose}>
-          close
-        </span>
-      </div>
-      <div className="field">
-        <label>Username</label>
-        <input
-          type="text"
-          name="username"
-          value={userData.username}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="field">
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          value={userData.password}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="field">
-        <button type="submit">Login</button>
-      </div>
-      {success && <div id="success">Successful!</div>}
-      {error && <div id="failure">Something went wrong</div>}
-    </form>
+        <div className="field">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="field">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={userData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="field">
+          <button type="submit">Login</button>
+        </div>
+        {success && <div id="success">Successful!</div>}
+        {error && <div id="failure">Something went wrong</div>}
+      </form>
+    </React.Fragment>
   );
 };
 
-export default Login;
+export default LoginForm;
