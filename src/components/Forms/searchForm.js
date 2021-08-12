a2e7e483e3aa76e22e714430be9fa4a797dd537d
T.S.Ajeet <codewithstatus200@gmail.com>
2021-08-13 00:47:53 +0530

    feat: unsplashjs api integration

--- src/components/Forms/searchForm.js
+++ src/components/Forms/searchForm.js
@@ -1,8 +1,18 @@
 import { useRef } from 'react';
 import axios from 'axios';
 import CountriesSelect from './CountriesSelect';
+import { createApi } from 'unsplash-js';
 
-const { REACT_APP_OPENCAGE_KEY, REACT_APP_OPENCAGE_API_FORWARD } = process.env;
+const {
+  REACT_APP_OPENCAGE_KEY,
+  REACT_APP_OPENCAGE_API_FORWARD,
+  REACT_APP_UNSPLASH_API,
+} = process.env;
+
+const unsplashAPI = new createApi({
+  accessKey: REACT_APP_UNSPLASH_API,
+  fetch: fetch,
+});
 
 const SearchForm = () => {
   const searchRef = useRef();
@@ -19,6 +29,17 @@ const SearchForm = () => {
       console.log(responseJSON.data.results[0].annotations);
       console.log(responseJSON.data.results[0].components);
       console.log(responseJSON.status);
+
+      const photos = await unsplashAPI.search.getPhotos({
+        query: searchQuery,
+        page: 1,
+        perPage: 5,
+        orderBy: 'relevant',
+      });
+
+      console.log(photos.response.results[0].urls.raw);
+      console.log(photos.response);
+      console.log(photos.status);
     } catch (error) {
       console.error(error);
     }
