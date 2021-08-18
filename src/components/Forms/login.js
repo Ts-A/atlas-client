74412bd20ccbc62503b4535c6d951e9ec5aaa3d8
T.S.Ajeet <codewithstatus200@gmail.com>
2021-08-18 20:12:32 +0530

    style: login form styled

--- src/components/Forms/login.js
+++ src/components/Forms/login.js
@@ -1,80 +1,81 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './login.css';
+import map from '../../images/map.jpg';
 
 const Login = (props) => {
   const { toggleShowLogin, loginUser } = props;
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
 
   const handleSubmit = (event) => {
     try {
       event.preventDefault();
 
-      if (userData.username.trim().length === 0)
-        throw new Error('Username field cannot be empty');
-      if (userData.password.length === 0)
-        throw new Error('Password field cannot be empty');
-      loginUser(userData);
-    } catch (error) {
-      setWarning((prev) => ({ ...prev, empty: false, message: error.message }));
-    }
-  };
-  const [userData, setUserData] = useState({
-    username: '',
-    password: '',
-  });
+      const username = usernameRef.current.value;
+      const password = passwordRef.current.value;
 
-  const handleInputChange = (event) => {
-    setWarning({ empty: true, message: '' });
-    setUserData((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
+      if (username.trim().length === 0)
+        throw new Error('Username field cannot be empty');
+      if (password.length === 0)
+        throw new Error('Password field cannot be empty');
+      usernameRef.current.value = '';
+      passwordRef.current.value = '';
+      loginUser({ username, password });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   const handleOnClose = () => {
     toggleShowLogin((prev) => ({ ...prev, login: false }));
   };
 
-  const [warning, setWarning] = useState({ empty: true, message: '' });
-
   return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      <div className="header">
-        <div>
-          Login
-          <span
-            className="material-icons"
-            style={{ fontSize: '32px', color: '#af1100' }}
-          >
-            room
-          </span>
-        </div>
-        <span className="material-icons close" onClick={handleOnClose}>
-          close
+    <div className="form-container">
+      <span className="close">
+        <span onClick={handleOnClose} className="material-icons">
+          cancel
         </span>
+      </span>
+      <div className="login-image">
+        <img src={map} alt="Globe" />
       </div>
-      <div className="field">
-        <label>Username</label>
-        <input
-          type="text"
-          name="username"
-          value={userData.username}
-          onChange={handleInputChange}
-        />
+      <div className="login-form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-heading">
+            <div>Login</div>
+          </div>
+          <div className="form-fields">
+            <div className="form-field">
+              <div>
+                <span class="material-icons label-icon">account_circle</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Username*"
+                name="username"
+                ref={usernameRef}
+              />
+            </div>
+            <div className="form-field">
+              <div>
+                <span class="material-icons label-icon">lock</span>
+              </div>
+              <input
+                type="password"
+                placeholder="Password*"
+                name="password"
+                ref={passwordRef}
+              />
+            </div>
+          </div>
+          <div className="form-submission">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
-      <div className="field">
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          value={userData.password}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="field">
-        <button type="submit">Login</button>
-      </div>
-      {!warning.empty && <div id="warning">{warning.message}</div>}
-    </form>
+    </div>
   );
 };
 
